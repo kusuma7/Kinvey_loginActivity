7c8c8130f07f0f6c23ad8b608276302a04db9007
kusuma mathi <kusuma mathi>
2016-08-15 17:01:55 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kavya/sample/KinveyActivity.java
@@ -0,0 +1,23 @@
+package com.example.kavya.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kinvey.android.Client;
+
+/**
+ * Created by KUSUMA on 8/9/2016.
+ */
+public class KinveyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public Client getClient(){
+       return ((MainApplication)getApplication()).getClient();
+    }
+}
+
---
+++ app/src/main/java/com/example/kavya/sample/MainApplication.java
@@ -0,0 +1,27 @@
+package com.example.kavya.sample;
+
+import android.app.Application;
+
+import com.kinvey.android.Client;
+
+/**
+ * Created by KUSUMA on 8/9/2016.
+ */
+public class MainApplication extends Application {
+
+   private Client client;
+    @Override
+
+    public void onCreate(){
+        super.onCreate();
+        defineClient();
+    }
+
+    private void defineClient() {
+         client = new Client.Builder("kid_H181PtvF","70541c2ff2b8466183762eee8bc4c4bb",getApplicationContext()).build();
+    }
+
+    public Client getClient(){
+        return client;
+    }
+}
